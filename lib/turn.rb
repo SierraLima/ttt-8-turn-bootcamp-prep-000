8877b2f595062f0fc84e6aeb19b20399e8fe8f7e
Stéphane <stephluethi@hotmail.com>
2018-06-19 14:04:07 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board,index)
-    move(board,index,) 
+    move(board,index) 
 end
 
 def display_board(board)
