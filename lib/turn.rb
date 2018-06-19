a376f53906f301e23855034ccb5e0dc12d89d93a
Stéphane <stephluethi@hotmail.com>
2018-06-19 14:23:43 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,7 +4,7 @@ def turn(board)
   index = input_to_index(user_input)
   
   if valid_move?(board,index) == true
-    move(board,index,"X") 
+    move(board,index,) 
   else 
     puts "Wrong input"
     turn(board)
